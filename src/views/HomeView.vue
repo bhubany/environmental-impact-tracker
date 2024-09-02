d40e858e90ee75518c1913007d7b000b77d1374d
bhubany <yadav.bhuban.by@gmail.com>
2024-09-02 22:20:40 +0545

    chore();setup tailwind css

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Hello from home view</h1>
 </template>
