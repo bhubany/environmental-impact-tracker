c2e282d17851312f60b8b7f80f0d0c2c42dd5f35
bhubany <yadav.bhuban.by@gmail.com>
2024-09-12 10:23:18 +0545

    chore(): setup pinia for state management

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,18 @@ import 'leaflet/dist/leaflet.css'
 import './assets/main.css'
 
 import { createPinia } from 'pinia'
+import piniaPluginPersistedState from 'pinia-plugin-persistedstate'
 import { createApp } from 'vue'
 
 import App from '@/App.vue'
 import router from '@/router'
 
 const app = createApp(App)
-// app.use(VueGoogleMaps, { load: { key: config.googleMapApiKey, libraries: 'visualization' } })
-app.use(createPinia())
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedState)
+app.use(pinia)
+
 app.use(router)
 
 app.mount('#app')
